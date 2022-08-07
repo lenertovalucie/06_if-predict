7e64a1f45616d99802c6328ee108ef14e93089a0
Lucie Lénertová <lenertova.lucie@gmail.com>
2022-08-07 08:46:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# nespouštějte program!
+# jaké tři řetězce se podle vás vytisknou do konzole? 
+# vaši předpověď napište do komentářů níže
+#
+#
+#
+
+age = 18 
+
+if age == 18:
+    print("You are old enough!") 
+
+num1 = 1337
+ 
+if num1 == 10: 
+    print("This text is output because the condition was true!")    
+else:
+    print("This text is output because the condition was false!") 
+
+print("This part is always printed!")
